d2753807ac323a0d773a47cdf7a45da8a943e7b5
acnilsen <acnilsen@stud.ntnu.no>
2026-01-23 12:07:07 +0100

    chore(homescreen): :art: Small changes to impove style and file structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useCameraPermissions } from "expo-camera";
 import { useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
-import { QRScanner } from "../components/QRScanner";
-import { Colors } from "../constants/Colors";
+import { QRScanner } from "../../components/QRScanner";
+import { Colors } from "../../constants/Colors";
 
 export default function Index() {
   const isReady = true; // Midlertidig hardkodet til true for testing, disable knapper hvis false
@@ -30,11 +30,11 @@ export default function Index() {
       <Text style={styles.title}>St. Olavs hospital</Text>
       <View style={styles.row}>
         <MaterialIcons name="star" size={24} color={Colors.brand.darkYellow} />
-        <Text style={styles.favourite}>Favoritt yrke</Text>
+        <Text style={styles.favourite}>Favorittyrke</Text>
       </View>
       <View style={styles.imageWrapper}>
         <Image
-          source={require("../assets/images/profile/1.png")}
+          source={require("../../assets/images/profile/1.png")}
           style={styles.image}
         />
       </View>
@@ -75,7 +75,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 20,
+    paddingTop: 80,
     alignItems: "center",
     backgroundColor: Colors.light.background || Colors.dark.background,
   },
