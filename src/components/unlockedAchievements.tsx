22bb0720e2f4edc6c67f26a69bf6c1f294ce56ad
acnilsen <acnilsen@stud.ntnu.no>
2026-01-27 09:54:08 +0100

    style(achievements): :lipstick: made a placeholder for the achievements page

---
+++ src/components/unlockedAchievements.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+// TODO: implement later
+export default function Achievements() {
+  return (
+    <View style={styles.container}>
+      <Text>Achievements Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
