0719dcb6cab98bbecf96cf395ed3836b69f3cdc8
Brahim Helland <brahimh@stud.ntnu.no>
2026-02-02 13:57:41 +0100

    style(admin): :lipstick: added new colors for placeholder text

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,39 +1,39 @@
-import { Background } from "@react-navigation/elements";
-
 const palette = {
-  darkBlue: '#003087',
-  lightBlue: '#6CACE4',
-  black: '#000000',
-  white: '#FFFFFF',
-  gray: '#BFCED6',
-  sage: '#6FA287',
-  lightGreen: '#ADDFB3',
-  turquoise: '#00C19F',
-  green: '#93C90E',
-  yellow: '#FFC845',
-  darkYellow: '#F7A700',
-  orange: '#FF671F',
-  brown: '#A76E5E',
-  red: '#CB333B',
-  purple: '#87189D',
+  darkBlue: "#003087",
+  lightBlue: "#6CACE4",
+  black: "#000000",
+  white: "#FFFFFF",
+  gray: "#BFCED6",
+  sage: "#6FA287",
+  lightGreen: "#ADDFB3",
+  turquoise: "#00C19F",
+  green: "#93C90E",
+  yellow: "#FFC845",
+  darkYellow: "#F7A700",
+  orange: "#FF671F",
+  brown: "#A76E5E",
+  red: "#CB333B",
+  purple: "#87189D",
 };
 
 export const Colors = {
   brand: palette,
-  
+
   light: {
     text: palette.black,
-    background: '#EFEEE6',
-    backgroundSecondary: '#D6D6D6',
+    background: "#EFEEE6",
+    backgroundSecondary: "#D6D6D6",
     accent: palette.darkBlue,
     button: palette.darkBlue,
     buttontext: palette.white,
+    placeholder: "#6C6C6C",
   },
   dark: {
     text: palette.white,
-    background: '#141414',
-    backgroundSecondary: '#2C2C2C',
+    background: "#141414",
+    backgroundSecondary: "#2C2C2C",
     button: palette.lightBlue,
     buttontext: palette.black,
+    placeholder: "#AFAFAF",
   },
 };
