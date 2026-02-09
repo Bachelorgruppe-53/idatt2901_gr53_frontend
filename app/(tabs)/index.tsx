dcda4eb9ac8d9edf20b9525a55a3bd21477e95f3
Brahim Helland <brahimh@stud.ntnu.no>
2026-02-09 10:42:55 +0100

    style: :lipstick: ran prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,12 @@
 import JoinClassModal from "@/src/components/joinClass";
+import { QRScanner } from "@/src/components/QRScanner";
+import { Colors } from "@/src/constants/Colors";
+import { useQRScanner } from "@/src/hooks/useQRScanner";
+import { useThemeColor } from "@/src/hooks/useThemeColor";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import axios from "axios";
-import { useCameraPermissions } from "expo-camera";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
-import { QRScanner } from "../../src/components/QRScanner";
-import { Colors } from "../../src/constants/Colors";
-import { useThemeColor } from "../../src/hooks/useThemeColor";
 
 /**
  * This page is the main landing page when the user opens the app.
@@ -22,7 +21,8 @@ import { useThemeColor } from "../../src/hooks/useThemeColor";
 export default function Index() {
   const theme = useThemeColor();
   const isReady = true; // Midlertidig hardkodet til true for testing, disable knapper hvis false
-  const { isScanning, startScanning, stopScanning, permissionError } = useQRScanner();
+  const { isScanning, startScanning, stopScanning, permissionError } =
+    useQRScanner();
   const [showJoinClass, setShowJoinClass] = useState(false);
   const [name, setName] = useState<string>("");
 
