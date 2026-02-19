12f636e01f4950462edb215d57a25079b37fa711
Brahim Helland <brahimh@stud.ntnu.no>
2026-02-19 10:53:18 +0100

    feat(stats): :sparkles: added new components for city, class and school wide scoreboards

---
+++ src/components/stats/classWideScoreboard.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ClassScoreboard() {
+  return (
+    <View>
+      <Text>Class Scoreboard</Text>
+    </View>
+  );
+}
---
+++ src/components/stats/schoolWideScoreboard.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SchoolScoreboard() {
+  return (
+    <View>
+      <Text>School Scoreboard</Text>
+    </View>
+  );
+}
