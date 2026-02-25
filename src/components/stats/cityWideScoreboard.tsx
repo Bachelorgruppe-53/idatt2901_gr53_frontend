0e424a5bd8399fd5f62b9aa19dd68764e2081477
Brahim Helland <brahimh@stud.ntnu.no>
2026-02-25 10:55:54 +0100

    feat(stats): :construction: added a temp citywide scoreboard with dummy data

--- src/components/stats/cityWideScoreboard.tsx
+++ src/components/stats/cityWideScoreboard.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Scoreboard from "./genericScoreboard";
 
 export default function CityScoreboard() {
   return (
-    <View>
-      <Text>City Scoreboard</Text>
+    <View style={styles.screen}>
+      <Scoreboard
+        entities={[
+          "Cissi Klein vgs",
+          "Thora Storm vgs",
+          "Malvik vgs",
+          "Rosenborg ungdomskole",
+          "Byåsen vgs",
+          "Nidelven ungdomskole",
+          "Strindheim videregående",
+          "Stavne ungdomskole",
+          "Trondheim vgs",
+          "Heimdal ungdomskole",
+          "Flatåsen videregående",
+          "Tiller ungdomskole",
+        ]}
+        scores={[1200, 950, 800, 750, 600, 580, 520, 480, 450, 420, 380, 340]}
+        scoreboardType="cityWide"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
