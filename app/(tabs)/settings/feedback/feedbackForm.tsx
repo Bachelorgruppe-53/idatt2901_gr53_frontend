d788f9f7b2a26aa8f0259816e87d48d0bbbabbf3
acnilsen <acnilsen@stud.ntnu.no>
2026-03-02 15:50:59 +0100

    fix(language): :speech_balloon: added translation to the feedback page

--- app/(tabs)/settings/feedback/feedbackForm.tsx
+++ app/(tabs)/settings/feedback/feedbackForm.tsx
@@ -1,10 +1,13 @@
 import { useThemeColor } from "@/src/hooks/useThemeColor";
 import { useThemedStyles } from "@/src/hooks/useStyleSheet";
+import { useTranslation } from "react-i18next";
 import { Linking, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function FeedbackForm() {
   const theme = useThemeColor();
   const themedStyles = useThemedStyles();
+
+  const { t } = useTranslation("settings");
   
   return (
     <View style={themedStyles.container}>
@@ -13,7 +16,7 @@ export default function FeedbackForm() {
         onPress={() => openURL("https://forms.gle/fRM6GnwfrvnoytVe6")}
       >
         <Text style={themedStyles.buttonText}>
-          Submit Feedback
+          {t("submitFeedback")}
         </Text>
       </Pressable>
     </View>
