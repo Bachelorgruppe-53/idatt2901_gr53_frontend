df81cb7cfc5aafd210a307f4ed0cb1e14539374b
acnilsen <acnilsen@stud.ntnu.no>
2026-04-09 13:49:53 +0200

    fix(admin): :fire: removed admin functionality

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,14 +1,11 @@
 import { Separator } from "@/src/components/Separator";
 import SettingsButton from "@/src/components/settingsButton";
 import { BaseStyles } from "@/src/constants/Styles";
-import { useAuth } from "@/src/context/AuthContext";
+import { useLeaveClass } from "@/src/hooks/useLeaveClass";
 import { useThemedStyles } from "@/src/hooks/useStyleSheet";
-import { useThemeColor } from "@/src/hooks/useThemeColor";
-import { Image } from "expo-image";
 import { useTranslation } from "react-i18next";
 import { Platform, ScrollView, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { useLeaveClass } from "@/src/hooks/useLeaveClass";
 
 /**
  * This page allows the user to change settings such as theme mode and language.
@@ -17,13 +14,9 @@ import { useLeaveClass } from "@/src/hooks/useLeaveClass";
  */
 
 export default function SettingsScreen() {
-  const theme = useThemeColor();
   const themedStyles = useThemedStyles();
-
   const { t } = useTranslation("settings");
   const insets = useSafeAreaInsets();
-
-  const { isAuthenticated } = useAuth();
   const { leaveClass } = useLeaveClass();
 
   return (
@@ -37,9 +30,6 @@ export default function SettingsScreen() {
     >
       <View style={themedStyles.container}>
         <View style={themedStyles.settingsSection}>
-          {/* <Text style={[styles.header, { color: theme.text }]}>
-            {t("settings")}
-          </Text> */}
           <View style={styles.imageWrapper}>
             <Text style={[themedStyles.heading, { marginTop: 20 }]}>
               {t("aboutUs")}
@@ -56,7 +46,6 @@ export default function SettingsScreen() {
           </Text>
         </View>
         <Separator />
-        {/* TODO: need to add a route here */}
         <SettingsButton
           route="/settings/feedback/feedbackForm"
           iconName="feedback"
@@ -74,7 +63,6 @@ export default function SettingsScreen() {
           </Text>
         </View>
         <Separator />
-        {/* Language selection button */}
         <SettingsButton
           route="/settings/lang/languageSelection"
           iconName="language"
@@ -87,36 +75,18 @@ export default function SettingsScreen() {
           labelKey="theme"
         />
         <Separator />
-        {/* Admin button, login if not authenticated, dashboard if authenticated */}
-        {!isAuthenticated && (
-          <SettingsButton
-            route="/settings/admin/login"
-            iconName="admin-panel-settings"
-            labelKey="admin"
-          />
-        )}
-        {isAuthenticated && (
-          <SettingsButton
-            route="/settings/admin/dashboard"
-            iconName="admin-panel-settings"
-            labelKey="dashboard"
-          />
-        )}
-        <Separator />
         <View style={themedStyles.settingsSection}>
           <Text style={[themedStyles.subheading, { marginVertical: 10 }]}>
             {t("settings")}
           </Text>
         </View>
         <Separator />
-        {/* TODO: need to add a route here */}
         <SettingsButton
           route="/settings/notifications/notificationSettings"
           iconName="notifications"
           labelKey="notificationSettings"
         />
         <Separator />
-        {/* TODO: need to add a route here */}
         <SettingsButton
           route="/settings/privacy/privacyPolicy"
           iconName="privacy-tip"
@@ -148,10 +118,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     overflow: "hidden",
     marginBottom: 20,
-  },
-  image: {
-    height: 200,
-    width: 200,
-    resizeMode: "contain",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,13 @@
-import { AuthProvider } from "@/src/context/AuthContext";
 import { ThemeProvider } from "@/src/context/ThemeContext";
 import "@/src/i18n/config";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </ThemeProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
