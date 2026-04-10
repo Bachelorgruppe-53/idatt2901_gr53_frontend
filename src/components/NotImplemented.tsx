2305f7b6ba07605dc9923d1e1928871af2fe4030
acnilsen <acnilsen@stud.ntnu.no>
2026-04-10 14:15:28 +0200

    feat: :sparkles: added a default not implemented yet page

---
+++ src/components/NotImplemented.tsx
@@ -0,0 +1,28 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import { useThemedStyles } from "../hooks/useStyleSheet";
+import { BaseStyles } from "../constants/Styles";
+import { useTranslation } from "react-i18next";
+
+interface NotImplementedProps {
+  title?: string;
+}
+
+export function NotImplemented({ title = "Coming Soon" }: NotImplementedProps) {
+  const themedStyles = useThemedStyles();
+  const { t } = useTranslation("common");
+
+  return (
+    <View style={[themedStyles.container, BaseStyles.center]}>
+      <MaterialIcons
+        name="upcoming"
+        size={64}
+        color={themedStyles.text.color}
+      />
+      <Text style={[themedStyles.modalTitle, { marginVertical: 8 }]}>{title}</Text>
+      <Text style={themedStyles.text}>
+        {t("notImplemented")}
+      </Text>
+    </View>
+  );
+}
--- src/components/unlockedAchievements.tsx
+++ src/components/unlockedAchievements.tsx
@@ -1,13 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import { useThemedStyles } from "../hooks/useStyleSheet";
+import { NotImplemented } from "./NotImplemented";
 
-// TODO: implement later
 export default function Achievements() {
-  const themedStyles = useThemedStyles();
-  
-  return (
-    <View style={themedStyles.container}>
-      <Text>Achievements Screen</Text>
-    </View>
-  );
+  return <NotImplemented title="Achievements" />;
 }
